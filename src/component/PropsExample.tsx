0a254a292c25d1ec5a0dd2b745f64815c37d96d1
sheraz khan <sheraz.0403889@talmeez.pk>
2022-04-04 22:05:16 +0500

    props done

--- src/component/PropsExample.tsx
+++ src/component/PropsExample.tsx
@@ -1,18 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
 interface IProps{
     name:string;
     contact:number;
+    
+}
+interface IState{
+
+    email:string;
 }
 const PropsExample:React.FC<IProps> = ({name , contact}) => {
+    let [state ,setState]=useState<IState>({email:'text@gmail.com'})
      return (
     <div><h2>PropsExample</h2>
     <div className="row">
         <div className="col">
             <div className="card">
                 <div className="card-body">
-                  <h4 >Name:{name}</h4>
-                  <h6 >Contact:{contact}</h6>
+                  <ul className="item-group">
 
+                      <li className="item">
+                        {name}
+                      </li>
+                      
+                      <li className="item">
+                        {contact}
+                      </li>
+                      <li className="item">
+                        {state.email}
+                      </li>
+                  </ul>
                 </div>
             </div>
         </div>
